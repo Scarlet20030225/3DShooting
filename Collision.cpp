b41f45fe9cd723ad1e635fafae042d00b85f7f85
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-02-12 17:18:33 +0900

    当たり判定処理のライブラリを追加

---
+++ Collision.cpp
@@ -0,0 +1,29 @@
+#include"Collision.h"
+
+namespace Lib
+{
+	Sphere::Sphere()
+		: mLocalCenter()
+		, mWorldCenter()
+		, mRadius(0.0f)
+	{
+	}
+
+	Sphere::Sphere(const VECTOR& center, float radius)
+		: mLocalCenter(center)
+		, mWorldCenter(center)
+		, mRadius(radius)
+	{
+	}
+
+	void Sphere::Move(const VECTOR& pos)
+	{
+		mWorldCenter = mLocalCenter + pos;
+	}
+
+	bool CollisionPair(const Sphere& sphere1, const Sphere& sphere2)
+	{
+		// ‹…‚Æ‹…“¯Žm‚Ì“–‚½‚è”»’è
+		return HitCheck_Sphere_Sphere(sphere1.mWorldCenter, sphere1.mRadius, sphere2.mWorldCenter, sphere2.mRadius);
+	}
+}
---
+++ Collision.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<DxLib.h>
+#include"Math.h"
+
+namespace Lib
+{
+	struct Sphere
+	{
+		Sphere();
+		Sphere(const VECTOR& center, float radius);
+		void Move(const VECTOR& pos);
+
+		VECTOR mLocalCenter;
+		VECTOR mWorldCenter;
+		float mRadius;
+	};
+
+	/// <summary>
+	/// “–‚½‚è”»’èƒyƒA
+	/// </summary>
+	bool CollisionPair(const Sphere& sphere1, const Sphere& sphere2);
+}
